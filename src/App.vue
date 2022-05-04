7df321716e42fc70e8bed0de3324bcdc793ceb31
rafajlpz <frilo_94@hotmail.com>
2022-05-04 13:17:33 +0200

    Creado archivo SCSS en assets
    Importacion de los archivos scss a ñps componentes

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,11 @@
 //Dependencias
 import TheMenu from "@/components/TheMenu.vue";
 import TheFooter from "@/components/TheFooter.vue"
+//Importacion de ficheros con Nodejs
+require("@/assets/scss/_reset.scss");
+require("@/assets/scss/estructura.scss");
 </script>
 
 <style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-}
 
-#app {
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-
-  padding: 0;
-  margin: 0;
-  min-height: 100vh;
-
-  background-color: white;
-
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
 </style>
